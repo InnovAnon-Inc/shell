474d1129756c52a9eb36f08e991fb5a2f8d7d84d
Default User <laurence.a.maddox@gmail.com>
2017-09-08 03:03:15 -0500

    added warning flags

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@ static int puts2 (const char *const str) {
 	return 0;
 }
 
-int main () {
+int main (void) {
 	/*
    char str0[] = "ls";
    char str1[] = "grep";
@@ -20,17 +20,17 @@ int main () {
    char *arg2[] = {str3};
    char **argvs[3] = {arg0, arg1, arg2};
    */
-   /*const*/ char /*const*/ * const * const * const argvs = (char **[]) {
-	 (char *[]) {"ls", NULL},
-	 (char *[]) {"grep", "shell", NULL},
-	 (char *[]) {"wc", NULL}  
+   /*const*/ char /*const*/ * const * const * const argvs = (char const * const *[]) {
+	 (char const *[]) {"ls", NULL},
+	 (char const *[]) {"grep", "shell", NULL},
+	 (char const *[]) {"wc", NULL}
    };
    size_t nargv = 3;
-   
+
    /*if (fork_and_wait (puts2, "Hello") != 0) return -2;*/
    /*if (fork_and_wait2 (puts2, "Hello", puts2, "World") != 0) return -2;*/
    /*puts ("main ()");*/
-   
+
    if (exec_pipeline (argvs, nargv) != 0) return EXIT_FAILURE;
    return EXIT_SUCCESS;
 }
